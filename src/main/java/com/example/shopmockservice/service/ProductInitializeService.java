88a1643bba1adee27534caac54988f9df3d37c0e
Damiangorskii <44735141+Damiangorskii@users.noreply.github.com>
2023-10-02 18:55:40 +0200

    minor adjustments for initial first app launch

--- src/main/java/com/example/shopmockservice/service/ProductInitializeService.java
+++ src/main/java/com/example/shopmockservice/service/ProductInitializeService.java
@@ -3,6 +3,7 @@ package com.example.shopmockservice.service;
 import com.example.shopmockservice.model.Product;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.PostConstruct;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -19,9 +20,10 @@ class ProductInitializeService {
     @PostConstruct
     public void init() {
         try {
-            ObjectMapper mapper = new ObjectMapper();
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
             InputStream inputStream = new ClassPathResource("products.json").getInputStream();
-            products = mapper.readValue(inputStream, new TypeReference<List<Product>>() {
+            products = objectMapper.readValue(inputStream, new TypeReference<List<Product>>() {
             });
         } catch (Exception e) {
             e.printStackTrace();
